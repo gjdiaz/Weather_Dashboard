12055a3a640f90a5805f069165d3f2b7138daa13
Geneva Diaz <kukla1422@gmail.com>
2021-09-08 08:27:31 -0500

    modifications to html and script

--- script.js
+++ script.js
@@ -1,22 +1,52 @@
 // borrowed from activity 9 Demo Dynamic
 // reference https://w3collective.com/fetch-display-api-data-javascript/
 // var weatherContainer = document.getElementById('weather');
-var fetchButton = document.getElementById('submit');
+var cityEl = document.getElementById('city');
+var dateEl = document.getElementById('date');
+var conditionsEl = document.getElementById('conditions');
+var tempEl = document.getElementById('temp');
+var humidEl = document.getElementById('humidity');
+var windEl = document.getElementById('wind');
+var uvEl = document.getElementById('uv');
+
+var dateDayOne = document.getElementById('date-dayOne')
+var descrDayOne = document.getElementById('descr-dayOne')
+var tempDayOne = document.getElementById('temp-dayOne')
+var humidDayOne = document.getElementById('humid-dayOne')
+var windDayOne = document.getElementById('wind-dayOne')
+var uviDayOne = document.getElementById('uvi-dayOne')
+
+var fetchButton = document.getElementById('submit')
 
 function getApi() {
     var requestUrl = 'https://api.openweathermap.org/data/2.5/onecall?lat=32.715&lon=-117.1625&appid=b07cfedc5a378ff86010f8868a197741';
 
-    fetch(requestUrl, {
-        method: 'GET',
-        credentials: 'same-origin',
-        redirect: 'follow',
-    })
+    fetch(requestUrl)
         .then(function (response) {
         return response.json();
         })
         .then(function (data) {
-        console.log(data);
-        });
-}
-fetchButton.addEventListener('click', getApi);
+        console.log(data);    
+        //cityEl.innerHTML = data.alerts[0].sender_name;
+        //dateEl.innerHTML = data.alerts[0].start;
+        conditionsEl.innerHTML = data.current.weather[0].description;
+        tempEl.innerHTML = data.current.temp;
+        humidEl.innerHTML = data.current.humidity;
+        windEl.innerHTML = data.current.wind_speed;
+        uvEl.innerHTML = data.current.uvi;
+        
+        dateDayOne.innerHTML = data.daily[0].dt;
+        descrDayOne.innerHTML = data.daily[0].weather[0].description;
+        tempDayOne.innerHTML = data.daily[0].temp.day;
+        humidDayOne.innerHTML = data.daily[0].humidity;
+        windDayOne.innerHTML = data.daily[0].weather.wind_speed;
+        uviDayOne.innerHTML = data.daily[0].uvi;
 
+
+        }); 
+
+}
+    fetchButton.addEventListener('click', function(e) {
+    e.preventDefault();
+    getApi();
+});
